326fb3addfbef9119f8a7c7cee576e7634ea29da
Aviva-Corcovado <171witb07@o365.htl-leoben.at>
2019-05-14 14:06:12 +0200

    initial commit

---
+++ src/at/htlle/shooter/gameobjects/Rocket.java
@@ -0,0 +1,61 @@
+package at.htlle.shooter.gameobjects;
+
+import javafx.geometry.Bounds;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.shape.Rectangle;
+
+public class Rocket 
+{
+	private int x;
+	private int y;
+	private String rocketString ="🚀";
+	
+	/**
+	 * ctor
+	 * @param x Startposition X 
+	 * @param y Startposition Y
+	 */
+	public Rocket(int x, int y)
+	{
+		this.x = x;
+		this.y = y;
+	}
+	
+	public void move()
+	{
+		this.x = this.x + 1;
+	}
+
+	/**
+	 * Überschreiben der toString Methode, damit wir den Asteroiden bequem
+	 * ausgeben können
+	 */
+	public String toString()
+	{
+		return "Rocket(" + this.x + "|" + this.y + ")";
+	}
+	
+	/**
+	 * Gibt true zurück wenn der Asteroid zerstört werden kann weil er nicht
+	 * mehr sichtbar ist
+	 * @return
+	 */
+	public boolean isVisible()
+	{
+		return (this.x < 1000);
+	}
+	
+	/**
+	 * Zeichnet die Rakete in den übergebenen GraphicsContext
+	 * @param gc
+	 */
+	public void paint(GraphicsContext gc)
+	{
+		gc.fillText(this.rocketString, this.x, this.y);
+	}
+	
+	public Bounds getBoeunds() {
+		Rectangle r = new Rectangle(this.x, this.y, 30, 10);
+		return r.getBoundsInLocal();
+	}
+}
---
+++ src/at/htlle/shooter/gameobjects/SpaceShip.java
@@ -0,0 +1,96 @@
+package at.htlle.shooter.gameobjects;
+
+import javafx.geometry.Bounds;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.shape.Rectangle;
+
+public class SpaceShip 
+{
+	private int x;
+	private int y;
+	private String shipString ="=8[]>";
+	
+	/**
+	 * ctor
+	 * @param x Startposition X 
+	 * @param y Startposition X 
+	 */
+	public SpaceShip(int x, int y)
+	{
+		this.x = x;
+		this.y = y;
+	}
+	
+	/**
+	 * Bewegt das Schiff nach Oben
+	 * @param delta Anzahl Pixel
+	 */
+	public void moveUp(int delta)
+	{
+		this.y = this.y - delta;
+	}
+	
+	/**
+	 * Bewegt das Schiff nach Unten
+	 * @param delta Anzahl Pixel
+	 */
+	public void moveDown(int delta)
+	{
+		this.y = this.y + delta;
+	}
+
+	/**
+	 * Bewegt das Schiff nach Links
+	 * @param delta Anzahl Pixel
+	 */
+	public void moveLeft(int delta)
+	{
+		this.x = this.x - delta;
+	}
+
+	/**
+	 * Bewegt das Schiff nach Rechts
+	 * @param delta Anzahl Pixel
+	 */
+	public void moveRight(int delta)
+	{
+		this.x = this.x + delta;
+	}
+
+	/**
+	 * Überschreiben der toString Methode, damit wir das RS bequem
+	 * ausgeben können
+	 */
+	public String toString()
+	{
+		return "SpaceShip(" + this.x + "|" + this.y + ")";
+	}
+	
+	/**
+	 * Zeichnet das Raumschiff in den übergebenen GraphicsContext
+	 * @param gc
+	 */
+	public void paint(GraphicsContext gc)
+	{
+		gc.fillText(this.shipString, this.x, this.y);
+	}
+	
+	/**
+	 * Feuert eine Rakete ab
+	 * @return
+	 */
+	public Rocket fireRocket()
+	{
+		return new Rocket(this.x, this.y);
+	}
+	 
+	public Bounds getBounds() {
+		Rectangle r = new Rectangle(this.x, this.y, 30, 10);
+		return r.getBoundsInLocal();
+	}
+
+	public void damage() {
+		this.shipString = "=>";
+	}
+	
+}
